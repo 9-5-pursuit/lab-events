c048cbef20781429bd29f9e9c9e4c6be04356aa9
Akira Watson-Brown <akirawatson-brown@new-host-4.home>
2023-03-15 14:28:29 -0400

    Events-lab-AB

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const curColor = document.querySelector("#current-color");
+const colorSelector = document.querySelectorAll(".color");
+
+for (let color of colorSelector) {
+  color.addEventListener("click", () => {
+    const selectedColor = color.getAttribute("style");
+    // console.log(`This button clicked! ${selectedColor}`);
+    curColor.setAttribute("style", `${selectedColor}`);
+    // console.log(curColor);
+  });
+}
+
+const cellCollection = document.querySelectorAll(".cell");
+
+for (let indivCell of cellCollection) {
+  // console.log(indivCell);
+  indivCell.addEventListener("click", () => {
+    //const selectCell = document.querySelector(".cell");
+    const colorInk = curColor.getAttribute("style");
+    indivCell.setAttribute("style", `${colorInk}`);
+  });
+}
