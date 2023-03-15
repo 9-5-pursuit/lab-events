1c11dab0e6d1d74b3b47b44e0d597cd5f77f1206
jeremytjimenez <jeremytjimenez@gmail.com>
2023-03-15 15:51:15 -0400

    specs passed

--- main.js
+++ main.js
@@ -7,3 +7,52 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+document.querySelector("body").style.background = "radial-gradient(white, azure)"
+
+let currentColor = document.querySelector("#current-color");
+let paletteColors = document.querySelectorAll(".color");
+let cells = document.querySelectorAll(".cell");
+
+function changeCurrentColor() {
+  for (let color of paletteColors) {
+    color.addEventListener("click", () => {
+      currentColor.style.background = color.style.background
+    })
+  }
+}
+
+for (let color of paletteColors) {
+  color.addEventListener("mousedown", changeCurrentColor);
+}
+
+function changeCellColor() {
+  for (let cell of cells) {
+    cell.addEventListener("click", () => {
+      cell.style.background = currentColor.style.background
+    })
+  }
+}
+
+for (let cell of cells) {
+  cell.addEventListener("mousedown", changeCellColor);
+}
+
+let button = document.createElement("button");
+button.setAttribute("type", "button");
+button.textContent = "Reset cell colors";
+button.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.background = "white";
+  }
+})
+document.querySelector("body").append(button);
+
+let button2 = document.createElement("button");
+button2.setAttribute("type", "button");
+button2.textContent = "Make all cells selected color";
+button2.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.style.background = currentColor.style.background;
+  }
+})
+document.querySelector("body").append(button2);
