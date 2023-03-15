8b393012b4c80fbe420d39ceba79a389e8c5b5f5
daleskiii <aun2793@gmail.com>
2023-03-15 16:05:07 -0400

    All tests Passed

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const colorPicked = document.querySelector("#palette");
+console.log(colorPicked);
+const currentColor = document.querySelector("#current-color");
+
+colorPicked.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("color")) {
+    const color = target.style.background;
+    currentColor.style.background = color;
+  }
+});
+/// #2
+
+const emptyBox = document.querySelectorAll(".cell");
+console.log(emptyBox);
+
+emptyBox.forEach((box) => {
+  box.addEventListener("click", () => {
+    box.style.background = currentColor.style.background;
+  });
+});
+
+//     const target2 = event.target
+
+//     target2.style.backgroundColor
+//   });
