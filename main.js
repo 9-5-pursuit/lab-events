82aa34afada934ac95003710ead932dd89fe4236
lgxxhi <luisgsilva95@gmail.com>
2023-03-15 23:55:53 -0400

    Complete Lab

--- main.js
+++ main.js
@@ -7,3 +7,39 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// Should change the background of the `#current-color` div when a color is clicked
+
+const colors = document.querySelectorAll(".color");
+let currentColor = document.querySelector("#current-color");
+//console.log(currentColor);
+
+for (let color of colors) {
+  color.addEventListener("click", () => {
+    let attribute = color.getAttribute("style");
+    //console.log(attribute);
+    currentColor.setAttribute("style", `${attribute}`);
+    //console.log(currentColor);
+  });
+}
+
+// Should change cells to the currently selected color when clicked
+
+let cells = document.querySelectorAll("#canvas div");
+
+for (let cell of cells) {
+  cell.addEventListener("click", () => {
+    let attribute = currentColor.getAttribute("style");
+    cell.setAttribute("style", `${attribute}`);
+  });
+}
+
+// Create a button that, when clicked, resets all cells so that they all have a background of white.
+
+const button = document.querySelector(".button");
+
+for (let cell of cells) {
+  button.addEventListener("click", () => {
+    cell.setAttribute("style", `background: white`);
+  });
+}
