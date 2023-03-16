abaea967ef3885e592e17f413080d3641ceb1953
ambarimar ulloa <ambarimar@ambarimars-Air.fios-router.home>
2023-03-16 10:41:20 -0400

    done

--- main.js
+++ main.js
@@ -9,24 +9,16 @@ for (let i = 0; i < 100; i++) {
 // You may write your code here!
  
   const current = document.querySelector("#current-color");
-  const colors = document.querySelectorAll(".color");
-
+const colors = document.querySelectorAll(".color");
   for (let color of colors){
 color.addEventListener("click", () => {
 current.style.background = color.style.background;
-});
-  }
+  });
+ }
 
-  // for (let color of colorsArray) {
-  //   color.addEventListener("click", () => {
-  //     currentColor.style.background = color.style.background;
-  //   });
-  // }
-
-  const cells = document.querySelectorAll(".cell") 
-  //console.log(cells)
+  const cells = document.querySelectorAll(".cell");
   for (let cell of cells) {
     cell.addEventListener("click", () => {
-      cell.style.background = current.style.background
-    })
+      cell.style.background = current.style.background;
+   });
   }
