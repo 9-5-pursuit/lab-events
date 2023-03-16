6890b7531474af777904e97f3fc2e326be40b9f1
Blui322 <luistejada@pursuit.com>
2023-03-16 10:30:23 -0400

    completed

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+ 
+const currentNeeded = document.querySelector("#current-color");
+const colors = document.querySelectorAll(".color");
+
+  for (let color of colors){
+color.addEventListener("click", () => {
+currentNeeded.style.background = color.style.background;
+  });
+ }
+
+  const cells = document.querySelectorAll(".cell"); 
+ 
+  for (let cell of cells) {
+    cell.addEventListener("click", () => {
+      cell.style.background = currentNeeded.style.background
+   });
+  }
+
+
+  
