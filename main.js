0f97dad27d5fd4c38f45319698a4e4027998759d
Ari-Collado <arielacollado@pursuit.org>
2023-03-27 12:43:55 -0400

    events

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+// listener
+// const paragraph = document.querySelector("p");
+// paragraph.addEventListener("click", () => {
+//   console.log("You are clicking the first paragraph on the page!");
+// });
+
+
+// add multiple listeners
+// const buttons = document.querySelectorAll("button");
+// for (let button of buttons) {
+//   button.addEventListener("click", () => {
+//     console.log("A button was clicked!");
+//   });
+// }
+// Adding event listeners dynamically-multiple list were added
+// const lis = document.querySelectorAll("li");
+// for (let li of lis) {
+//   li.addEventListener("click", () => {
+//     const ol = document.querySelector("ol");
+//     const newLi = document.createElement("li");
+//     newLi.textContent = "Click me to make more!";
+//     ol.append(newLi);
+//   });
+// }
+
+
+//canvas-cuurentcolor-palette-color
+//1. query select current color then actual color 
+//2.qs individual color
+//3.run loop as list example above no consolelog change background
+let currentColor = document.querySelector('current-color')
+
+let colors = document.querySelectorAll(.color)
+
+for (const color of colors){
+ color.addEventListener('click,() =>'{
+  currentColor.style.background = color.style.background
+ })
+}
