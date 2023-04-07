b5dab04206ab9d1a5412c11c4c310c0a1204977e
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2023-04-07 17:30:36 -0400

    done

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const currentColor = document.getElementById("current-color");
+const paletteColors = document.querySelectorAll("#palette .color");
+
+for (let color of paletteColors) {
+  color.addEventListener("mousedown", () => {
+    const selectedColor = color.getAttribute("style");
+    currentColor.setAttribute("style", selectedColor);
+  });
+}
+
+const cells = document.querySelectorAll(".cell");
+  for (let cell of cells) {
+    cell.addEventListener("click", () => {
+      cell.setAttribute("style", "background:" + currentColor.style.background)
+    })
+  }
